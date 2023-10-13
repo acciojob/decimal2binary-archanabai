542eb54561987b080408f9f97fa97e6fef164b74
archanabai <121252186+archanabai@users.noreply.github.com>
2023-10-13 17:14:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function decimalToBinary(num) {
   //Write you code here
   let binaryNum ="";
 	while(num > 0){
-		rem = num % 2;
+		let rem = num % 2;
 		binaryNum +=rem;
 		num =Math.floor( num/2);
 	}
